322049a277122834b4135b3e03a9dc1cb3077d81
董坤鸿 <249529979@qq.com>
2020-06-23 17:54:27 +0800

    修改配置文件

--- src/config.php
+++ src/config.php
@@ -5,14 +5,14 @@
  * Date: 2020/06/23
  * Time: 14:39
  */
-namespace invoice\src;
 
+namespace Buqiu\Invoice;
 
 return [
     'DSPTBM' => 'P1000001',                 //'电商平台编码'
     'NSRSBH' => '913101010000000090',          //'纳税人识别码'
-    'NSRMC'  => 'XXX官方旗舰店',             //'纳税人名称',
-    'XHFMC'  => 'XXX官方旗舰店',              //'销货方名称'
+    'NSRMC' => 'XXX官方旗舰店',             //'纳税人名称',
+    'XHFMC' => 'XXX官方旗舰店',              //'销货方名称'
     'XHF_DZ' => '上海市徐汇区XXX号',  //'销货方地址',
     'XHF_DH' => '17621251***',              //'销货方电话',
     'XHF_YHZH' => '87878787878788787878787',  //'销货方银行账号',
@@ -23,7 +23,7 @@ return [
     'APPID' => 'ZZS_PT_DZFP',
     'TAXPAYWERID' => '913101010000000090',     //'税号',
     'AUTHORIZATIONCODE' => 'NH873FG4KW',      //'授权码',
-    'ENCRYPTCODE' =>'2', //0:不加密 1: 3DES 加密 2:CA
+    'ENCRYPTCODE' => '2', //0:不加密 1: 3DES 加密 2:CA
     'INTERFACE_FPKJ' => 'ECXML.FPKJ.BC.E_INV',
     'INTERFACE_FPXZ' => 'ECXML.FPXZ.CX.E_INV',
     'INTERFACE_FPYX' => 'ECXML.EMAILPHONEFPTS.TS.E.INV',
